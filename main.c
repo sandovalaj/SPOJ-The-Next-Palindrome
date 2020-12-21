f4a61d68e9331b79660807fd0c76f982068bebdb
Anne <sandovalaj0987@gmail.com>
2020-12-21 16:26:21 +0800

    Optimized code Reversed input using math process instead of typecasting to char Solution still not accepted by SPOJ

--- main.c
+++ main.c
@@ -1,71 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-#include <math.h>
-
-int getSize(int input){
-    unsigned char Carray[UCHAR_MAX];
-    sprintf(Carray, "%d", input);
-    int size=strlen(Carray);
-    return size;
-    }
-
-int integerReverse(int input, int size){
-    int array[size];
-    int final=0;
-
-    for(int a=0; a<size; a++){
-        array[a]=input%10;
-        input=input/10;
-    }
-    int fin=0;
-    int sizeHolder=size;
-
-    for(int b=0; b<size; b++){
-        int addMe=array[b]*(pow(10, (sizeHolder-1)));
-        fin=fin+addMe;
-        sizeHolder--;
-    }
-
-    return fin;
-    }
-
+#include<stdio.h>
+#include<stdlib.h>
 
 int main()
 {
+    //reversing an integer
     int t;
 
-    printf("A positive integer is called a palindrome if its representation in the decimal system is the same when read from left to right and from right to left. For a given positive integer K of not more than 1000000 digits, write the value of the smallest palindrome larger than K to output. Numbers are always displayed without leading zeros.\n");
-    printf("\nInput t: ");
-    scanf("%d", &t);
-
+    //printf("Input t: ");
+    scanf("%u", &t);
     int array[t];
-    int finalArray[t];
 
-    for(int a=0; a<t;a++){
-        scanf("%d", &array[a]);
+    //takes t numbers of input
+    for(int a=0; a<t; a++){
+        scanf("%u", &array[a]);
         array[a]++;
     }
 
-    int b=0;
-    while(b!=t+1){
-        int size=getSize(array[b]);//size of integer
-        int fin=integerReverse(array[b], size);//reversed integer
-        if(fin==array[b]){ //if reversed integer is equal to original input
-            finalArray[b]=array[b]; //assign to an array
-            b++;
-        //carry on to get other input
-        }else{ //else add one to original input
-            array[b]++; //repeat
+    printf("\n");
+
+    for(int b=0; b<t; b++){
+        int copy=array[b];
+        int check=0;
+
+        while(check!=1){
+            array[b]=copy;
+            int digit=0;
+            int Rinput=0;
+
+            while(array[b]!=0){ //reverses digits of input
+                digit=array[b]%10;
+                Rinput=Rinput*10+digit;
+                array[b]/=10;
+                }
+
+            if(copy==Rinput){ //if the reversed input and original input is equal
+                printf("%d\n", Rinput);
+                check++;
+            }else{
+                copy++; //plus one to copy then repeat whole process
+            }
         }
-    }
-
-    for(int c=0; c<t;c++){
-        printf("\n%d", finalArray[c]);
-    }
-
-
-
+      }
     return 0;
 }
-
